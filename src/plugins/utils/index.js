daf60d7c7f0c81269cea1203d0fff361c112a1ca
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-05-29 17:46:14 +0430

    feat(plugin/utils): change regex
    some minor changes in regex in plugin/utils

--- src/plugins/utils/index.js
+++ src/plugins/utils/index.js
@@ -1,12 +1,13 @@
+const regex = /\(|\)|\%|\.|\#|\,|\+|\:|\"|\/|\/|\`|\'/g;
 const createModuleComposer = (property, value) => {
-  return (`\t@composes: ${property}--${value.replace(/ /g,'-').replace(/\(|\)|\%|\./g,'')} from 'styles.scss;'\n`);
+  return (`\t@composes: ${property}--${value.replace(/ /g,'-').replace(regex,'')} from 'styles.scss;'\n`);
 }
 const createModuleDeclaration = (property, value) => {
   return (`\t${property}: ${value};\n`);
 }
 
 const createModuleSelector = (property, value) => {
-  return (`.${property}--${value.replace(/ /g,'-').replace(/\(|\)|\%|\./g,'')} {\n`);
+  return (`.${property}--${value.replace(/ /g,'-').replace(regex,'')} {\n`);
 }
 
 module.exports = {
